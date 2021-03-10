5ed7a3353955fb1cf429fc8e0b905efda2ce23c0
proteeti13 <proteeti13@gmail.com>
2021-03-10 15:37:48 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WrappedStudyBrowser from './WrappedStudyBrowser.js';
+
+function App() {
+  return (
+    <div className="App">
+     <WrappedStudyBrowser/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WrappedStudyBrowser.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { DndProvider } from 'react-dnd';
+import {TouchBackend} from 'react-dnd-touch-backend';
+
+//
+import {
+  studies,
+  onThumbnailClick,
+  onThumbnailDoubleClick,
+} from './exampleStudies.js';
+import {ExampleDropTarget} from './ExampleDropTarget.js';
+import { StudyBrowser } from './StudyBrowser.js';
+
+class StudyBrowserContainer extends Component {
+  render() {
+    //const viewportData = [null, null, null, null];
+
+    return (
+      <React.Fragment>
+        <ExampleDropTarget />
+        <StudyBrowser
+          studies={studies}
+          onThumbnailClick={onThumbnailClick}
+          onThumbnailDoubleClick={onThumbnailDoubleClick}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+// Note:
+// Normally, the top level APP component is wrapped with the DragDropContext
+// We wrap this component to create a simple/local example.
+const WrappedStudyBrowser = DndProvider(
+  TouchBackend,
+  { enableMouseEvents: true },
+  null,
+  true
+)(StudyBrowserContainer);
+
+// http://react-dnd.github.io/react-dnd/docs/api/drag-drop-context
+export default { WrappedStudyBrowser };
